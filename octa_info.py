5454642770de16daf26c00d70ccda549baa37973
brulo <bruce.lott@gmail.com>
2020-03-25 16:13:06 -0700

    using curses now

--- octa_info.py
+++ octa_info.py
@@ -1,26 +1,30 @@
 import os.path
 import keyboard
+import curses
 
+screen = curses.initscr()
+screen.clear()
+screen.refresh()
 
 def printStuff(programIndex):
     f = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "{0}.txt".format(programIndex)), "r")
     content = f.read()
-    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
-    print("PROGRAM {0}".format(programIndex))
-    print("-----------------------------------------------")
-    
-    print(content)
-    numNewLines = 9 - content.count('\n')
-    while numNewLines > 0:
-        print("\n")
-        numNewLines -= 1
+    screen.clear()
+    screen.addstr("PROGRAM {0}\n".format(programIndex))
+    screen.addstr("-------------------------------------\n")
+    screen.addstr(content)
+    screen.refresh()
 
 printStuff(0)
 
 while True:
+    curses.napms(1)
     if keyboard.is_pressed('a'):
         printStuff(0)
 
     if keyboard.is_pressed('s'):
         printStuff(1)
 
+    if keyboard.is_pressed('q'):
+        curses.endwin()
+        break
